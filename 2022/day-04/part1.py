c09d9180fc95a0839d318e634558a2fa4be53be8
Eduardo Apolinario <eapolinario@users.noreply.github.com>
2022-12-05 23:49:39 -0800

    2022 - day 04
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

---
+++ 2022/day-04/part1.py
@@ -0,0 +1,31 @@
+import sys
+from typing import Tuple
+
+def parse_assignment(assignment_str: str) -> Tuple:
+    assignment = assignment_str.split('-')
+    return int(assignment[0]), int(assignment[1])
+
+def parse_line(line: str) -> Tuple[Tuple[int], Tuple[int]]:
+    assignments = line.split(',')
+    return parse_assignment(assignments[0]), parse_assignment(assignments[1])
+
+def cover(a: Tuple[int, int], b: Tuple[int, int]):
+    return (a[0] <= b[0] and a[1] >= b[1]) or (b[0] <= a[0] and b[1] >= a[1])
+
+
+def main():
+    assignments = []
+    for line in sys.stdin:
+        line = line.rstrip()
+        assignments.append(parse_line(line))
+
+    ans = 0
+    for assignment in assignments:
+        if cover(assignment[0], assignment[1]):
+            ans += 1
+
+    print(ans)
+
+
+if __name__ == '__main__':
+    SystemExit(main())
